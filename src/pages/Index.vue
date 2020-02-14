a9a7c9255d534afc06e583cda6bfe93d5377e4a1
Nikudo-4 <kunach1lo@gmail.com>
2020-02-14 10:41:25 +0300

    some shit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,28 @@
 <template>
   <div>
   <router-link to="/week"><h3>Электронный журнал</h3></router-link>
-    <span >Школа № 30</span>
+      <span >Школа № 30</span>
+    <q-btn @click="scan">Сканировать QR</q-btn>
   </div>
-
 </template>
 
 <script>
+export default{
+methods:{
+ scan(){
+   cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
+ }
+}
+}
+  
 </script>
